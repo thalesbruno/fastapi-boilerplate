727acd4574241c6262aa209c79ece3749e99f244
Thales Bruno <thalesbrunom@gmail.com>
2020-08-17 06:12:53 -0300

    create crud package

--- backend/crud/crud.py
+++ backend/crud/crud.py
@@ -3,19 +3,19 @@ from models import models
 from schemas import schemas
 
 
-async def get_user(db: Session, user_id: int):
+def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
-async def get_user_by_email(db: Session, email: str):
+def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
 
-async def get_users(db: Session, skip: int = 0, limit: int = 100):
+def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
-async def create_user(db: Session, user: schemas.UserCreate):
+def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = models.User(
         email=user.email, hashed_password=fake_hashed_password)
@@ -25,11 +25,11 @@ async def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 
-async def get_items(db: Session, skip: int = 0, limit: int = 100):
+def get_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Item).offset(skip).limit(limit).all()
 
 
-async def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
     db_item = models.Item(**item.dict(), owner_id=user_id)
     db.add(db_item)
     db.commit()
--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database.setup import SessionLocal, engine
 from models import models
+from schemas import schemas
+from crud import crud
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -11,9 +14,14 @@ app = FastAPI()
 # Dependecy
 
 
-async def get_db():
+def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+@app.get("/")
+async def read_root():
+    return {"Response": "Hello World"}
