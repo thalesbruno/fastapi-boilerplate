102d6097e92d6fae8de8d652cb9053ecd92f22c0
Thales Bruno <thalesbrunom@gmail.com>
2020-08-22 20:09:13 -0300

    update user tests

--- backend/app/api/routers/users.py
+++ backend/app/api/routers/users.py
@@ -36,24 +36,24 @@ def read_users_me(
     return current_user
 
 
-@router.post("/users", response_model=schemas.User, tags=['users'])
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db=db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
 @router.get("/users", response_model=List[schemas.User], tags=['users'])
 def read_users(
         skip: int = 0, limit: int = 100,
-        db: Session = Depends(get_db),
-        token: str = Depends(oauth2_scheme)):
+        db: Session = Depends(get_db)):
     users = crud.get_users(db=db, skip=skip, limit=limit)
     return users
 
 
-@router.get("/users/{user_id}", response_model=schemas.User, tags=['users'])
+@ router.post("/users", response_model=schemas.User, tags=['users'])
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db=db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400,
+                            detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@ router.get("/users/{user_id}", response_model=schemas.User, tags=['users'])
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if db_user is None:
@@ -61,12 +61,12 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@router.post("/users/{user_id}/items", response_model=schemas.Item, tags=['users'])
+@ router.post("/users/{user_id}/items", response_model=schemas.Item, tags=['users'])
 def create_item_for_user(item: schemas.ItemCreate, user_id: int, db: Session = Depends(get_db)):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@router.delete("/users/{user_id}", tags=['users'])
+@ router.delete("/users/{user_id}", tags=['users'])
 def remove_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if not db_user:
