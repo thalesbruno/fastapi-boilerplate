e258ba9138f373629b7ef641142a8cad6aa36cce
Thales Bruno <thalesbrunom@gmail.com>
2020-09-02 14:45:22 -0300

    Add settings default values

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -4,8 +4,8 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str = "FastAPI Boilerplate"
 
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
+    POSTGRES_USER: str = "app"
+    POSTGRES_PASSWORD: str = "app"
     POSTGRES_SERVER: str = "db"
     POSTGRES_DB: str = "app"
 
