0e646dea825da7c271bc1d5a5fbcce5ba9cd219a
Thales Bruno <thalesbrunom@gmail.com>
2020-09-04 07:59:08 -0300

    Remove db creation on tests setup

--- backend/app/app/tests/setup.py
+++ backend/app/app/tests/setup.py
@@ -21,7 +21,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 
 def override_get_db():
